57418b5d6526549fbaae0dae7769b18c04fa9786
full710 <113315804+full710@users.noreply.github.com>
2023-10-09 14:57:56 -0300

    Add app Home/publicaciones/comunidad

--- project/config/settings.py
+++ project/config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'publicaciones',
+    'comunidad',    
 ]
 
 MIDDLEWARE = [
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
--- project/config/urls.py
+++ project/config/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path("", include("home.urls")),
+    path("cliente/", include("cliente.urls")),
+    path("productos/", include("productos.urls")),
+    path("envios/",include("envios.urls"))
+    
 ]
